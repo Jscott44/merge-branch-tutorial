347c9e792e6ac7e466c0ec3c251f21da3dc72a8d
Jed Scott <jedscott50@gmail.com>
2024-01-16 17:07:43 +0000

    Feature complete and tested

--- foo.h
+++ foo.h
@@ -5,10 +5,13 @@
 struct Foo
 {
     int n;
+    float p;
     Foo()
     {
         std::cout << "Enter n: "; // no flush needed
         std::cin >> n;
+        std::cout << "Enter p: "; // no flush needed
+        std::cin >> p;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ Foo f; // static object
 
 int main()
 {
-	std::cout << "Hi I am Version 4!\n";
-	std::cout << "f.n is " << f.n << '\n';
+	std::cout << "Hi I am Version 5!\n";
+	std::cout << "n times p is " << f.n*f.p<< '\n';
 }
 
