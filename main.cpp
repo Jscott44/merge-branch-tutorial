0639ae569790bf326bc95fcc6f160001c0b4a111
Jed Scott <2466460S@student.gla.ac.uk>
2024-01-22 08:55:03 +0000

    User 2 test change

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ Foo f; // static object
 
 int main()
 {
-	std::cout << "Hi I am Version 5!\n";
+	std::cout << "Hi I am Version 5!\n"; //test change
 	std::cout << "n times p is " << f.n*f.p<< '\n';
 }
 
